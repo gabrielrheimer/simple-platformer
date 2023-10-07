a4dbcc92ee1a43fbd413e7f7c8cf61cc49532685
gabrielrheimer <gabrielrh47@gmail.com>
2023-10-07 02:28:40 -0300

    New Canvas class

---
+++ src/Canvas.ts
@@ -0,0 +1,19 @@
+import { canvas } from ".";
+
+export class Canvas {
+    public static WIDTH = 1000;
+    public static HEIGHT = 400; 
+    
+    public static getCanvas() {
+        return document.getElementById("canvas-new") as HTMLCanvasElement;
+    }
+
+    public static getContext() {
+        return canvas.getContext("2d");
+    }
+    
+    public static setSize() {
+        canvas.width = Canvas.WIDTH;
+        canvas.height = Canvas.HEIGHT;
+    }
+}
