e0f010c34b4f4efc099f7b13965e23d682399993
gabrielrheimer <gabrielrh47@gmail.com>
2023-10-09 01:28:40 -0300

    Add speed and acceleration concepts

--- src/Player.ts
+++ src/Player.ts
@@ -1,5 +1,6 @@
 import { context } from ".";
 import { Colors } from "./Colors";
+import { IElement } from "./Scenario";
 
 let player: Player = undefined;
 
@@ -7,11 +8,25 @@ export class Player {
     /** @see Player.getInstance should be called to get Singleton instance of Player */
     private constructor () { player = this }
 
-    private height = 25;
-    private width = 25;
-    private posX = 200;
-    private posY = 200;
-    private color = Colors.CHARACTER_GREEN;
+    public height = 25;
+    public width = 25;
+    public posX = 200;
+    public posY = 200;
+    public color = Colors.CHARACTER_GREEN;
+
+    private element: IElement = {
+        height: this.height,
+        width: this.width,
+        posX: this.posX,
+        posY: this.posY,
+        color: this.color
+    };
+
+    public maximumSpeed = 5;
+    public speedY = 1;
+    public speedX = 1;
+    public accelerationX = 0.4;
+    public accelerationY = 0.4;
     
     /** @returns Singleton instance of Player */
     public static getInstance(): Player {
@@ -22,20 +37,26 @@ export class Player {
         }
     }
 
+    public getElement(): IElement {
+        return this.element;
+    }
+
     public drawPlayer(): void {
         context.fillStyle = this.color;
         context.fillRect(this.posX, this.posY, this.width, this.height);
     }
 
+    public resolveCollision(): void {
+        //
+    }
+
     public moveRight(): void {
-        this.posX += 1;
+        this.posX += this.speedX;
+        if (this.speedX < this.maximumSpeed) this.speedX += this.accelerationX;
     }
 
     public moveLeft(): void {
-        this.posX -= 1;
-    }
-
-    public moveDown(): void {
-        this.posY -= 1;
+        this.posX -= this.speedX;
+        if (this.speedX < this.maximumSpeed) this.speedX += this.accelerationX;
     }
 }
